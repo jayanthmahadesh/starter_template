90db875ae84c4e9d70355d87ddf2f82b95c83178
jayanthmahadesh <jayanthmahadesh777@gmail.com>
2023-01-26 14:46:19 +0530

    second

--- testing/urls.py
+++ testing/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage,name='homepage'),
 ]
-# if settings.DEBUG:
-# 	urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-# urlpatterns += staticfiles_urlpatterns() 
+if settings.DEBUG:
+	urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns() 
 
